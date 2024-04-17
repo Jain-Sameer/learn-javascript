96ac6a93390a88634147b51cdc49affc145aba09
Sameer Jain <118839709+Jain-Sameer@users.noreply.github.com>
2024-04-17 06:09:54 +0000

    datatypes

---
+++ datatypes/1st.js
@@ -0,0 +1,21 @@
+"use strict"; //Treat all code as newer JS code
+
+// alert (3 + 3)  this syntax works in browser, not in node.js
+
+let fname = "Sameer"
+let age = 20
+let isMarried = false
+
+//  Above are examples of datatypes
+
+// number = 2^53 range
+// big int
+// string = "place text here"
+// bool = true or false 
+// null -> standalone value , type of null is object
+// undefined -> when no value assigned , unique type => (undefined)
+// symbol -> unique
+
+// object 
+
+console.log(typeof(undefined));
