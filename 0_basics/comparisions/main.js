902abe062308d1d1f852bba879269a51d0a533e6
Sameer Jain <118839709+Jain-Sameer@users.noreply.github.com>
2024-04-17 06:55:49 +0000

    comparisons further study

---
+++ 0_basics/comparisions/main.js
@@ -0,0 +1,16 @@
+// console.log(2 < 1);
+// console.log(2 != 1);
+// console.log(1 == 1);
+
+
+// console.log("2" > 1);
+// console.log(2 < "1");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);  
+// equality check adn other comparisons work differently, compariosns convert null to a number, making it 0, thats why we got true ans for >= and false for >.
+
+// strict check - "===" , doesnt convert the datatypes in conversion
+
+console.log("2" === "2");
