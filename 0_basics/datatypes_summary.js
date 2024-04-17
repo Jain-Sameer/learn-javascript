a5eb5dcb616a98110dec6fd573b19cd0862b7d19
Sameer Jain <118839709+Jain-Sameer@users.noreply.github.com>
2024-04-17 12:47:38 +0000

    summary on datatypes

---
+++ 0_basics/datatypes_summary.js
@@ -0,0 +1,27 @@
+// Primitve
+// 7 types : String, Number, Boolean, null,. undefined, symbol, BigInt
+
+//  Is JS a dynamic or static typed? Dynamically Typed
+let f_id = 65
+
+let outsideTemp = null
+
+let isMarried = false
+
+let gender // undefined
+let gender_2 = undefined //still undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+
+// Reference Type (Non Primtiive)
+// Arrays, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name : "hitesh"
+}
