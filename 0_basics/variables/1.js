c5411b0ab9cf95ca95ceff9e5768f82a75081965
Sameer Jain <118839709+Jain-Sameer@users.noreply.github.com>
2024-04-17 05:56:39 +0000

    first setup

---
+++ 0_basics/variables/1.js
@@ -0,0 +1,13 @@
+const accountID = 144553;
+
+let accoutnEmail = "sameer@gmail.com";
+var password = "65434324";
+
+accountCity = "Patiala";
+let accountState
+// accountID = 2421421;    
+// Prefer not to use (var) for declaring variables bcz of block and scope issues
+
+console.log(accountID);
+
+console.table([accountID, accoutnEmail,password, accountCity,accountState ])
