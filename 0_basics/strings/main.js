f5aee8c8983fb7f4c1babdf82e222fd3afde7713
Sameer Jain <118839709+Jain-Sameer@users.noreply.github.com>
2024-04-17 13:27:22 +0000

    studied about strings and string functions

---
+++ 0_basics/strings/main.js
@@ -0,0 +1,22 @@
+const name = "Sameer"
+const repoCount = 50
+
+console.log(name + repoCount + " hahaha"); // Not prefered, ugly
+
+console.log(`Ny name : ${name} and my repo count is ${repoCount}`); // use back tix `` instead of adding up the values
+
+
+const gameName = new String('WOWOOWW') //Created an object using new keyword, heap used.
+// console.log(typeof name);
+// console.log(typeof gameName);
+
+// console.log(gameName.toLowerCase());
+
+// trim(), slice(), substring(), replace()
+
+let newName = "sameer-jain-123421"
+
+console.log(newName.split('-'));
+
+newName = "Sameer Jain"
+console.log(newName.blink());
