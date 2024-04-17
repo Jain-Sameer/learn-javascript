667428626e822db7b867168ad3fb5806238ee8f6
Sameer Jain <118839709+Jain-Sameer@users.noreply.github.com>
2024-04-17 06:44:07 +0000

    logic to conversion

--- 0_basics/datatypes/1st.js
+++ 0_basics/datatypes/1st.js
@@ -22,13 +22,31 @@ let isMarried = false
 
 let score = "33as"
 
-console.log(typeof score); // string output
+// console.log(typeof score); // string output
 
 let value_num = Number(score)
 
-console.log(typeof value_num); // number output
-console.log(value_num); // value is "NaN"
+// console.log(typeof value_num); // number output
+// console.log(value_num); // value is "NaN"
 
 // "33" -> 33
 // "33dsafsd" -> NaN
 // "true" -> 1
+
+// operations on datatypes
+
+let value = 3
+let negValue = -value;
+
+// console.log(2-"2");
+
+let result = (2+"1" == "2" + 1)
+// console.log(result);
+
+
+console.log(1+ 2+ "3")
+console.log("1"+ 2+ 3)
+
+// The Login behind this conversion or any coversion of datatypes can be read here : https://tc39.es/ecma262/#sec-toprimitive
+
+ 
