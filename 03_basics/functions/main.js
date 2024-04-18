40cf96b18beb5671e0c3d63b5dbf491500db2aac
Sameer Jain <118839709+Jain-Sameer@users.noreply.github.com>
2024-04-18 08:07:24 +0000

    learning about functions basics

---
+++ 03_basics/functions/main.js
@@ -0,0 +1,51 @@
+function sayMyName(){
+    console.log("S");
+    console.log("A");
+    console.log("M");
+    console.log("E");
+    console.log("E");
+    console.log("R");
+}
+
+// sayMyName()
+
+function add(a, b){
+    return a+b
+}
+
+// ... also spread also rest, combined all arguments passed into an array
+
+// example :
+
+function calculatePrice(...num1){
+    return num1
+}
+
+console.log(calculatePrice(100,2000,3000,40000));
+
+
+function CalculatePrice(a,b,...num1){
+    return num1
+}
+console.log(CalculatePrice(100,2000,3000,40000));
+
+// the rest operator takes values after a and b and combined the remaining into an array in this case only 3000 and 40000.
+
+const user = {
+    name: "Sameer",
+    Age: 20
+}
+
+function handleObj(anyobject){
+    console.log(`Name : ${anyobject.name} and Age : ${anyobject.age}`);
+}
+
+// handleObj(user)
+handleObj(
+    {
+        name: "Howowowo",
+        age: 21
+    }
+)
+
+// We can also pass array
