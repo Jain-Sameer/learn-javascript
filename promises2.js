cc9c043367526f27b24aced5781874fdc9adc2c5
Sameer Jain <118839709+Jain-Sameer@users.noreply.github.com>
2024-05-10 15:49:24 +0000

    promises fetch

--- promises2.js
+++ promises2.js
@@ -1,9 +1,24 @@
-async function getUsers(){
-    try {
-        const response = await fetch('https://api.github.com/users/hiteshchoudhary');
-        const data = response.json();
-        console.log(data);
-    } catch (error) {
-        console.log('E : ' + error);
-    }
-}
+
+// async function getUsers(){
+//     try {
+//         const response = await fetch('https://api.github.com/users/hiteshchoudhary');
+//         // console.log(response);
+//         const data = await response.json(); // response.json() takes time to process that is why we need to await it also
+//         console.log(data); 
+//     } catch (error) {
+//         console.log('E : ' + error);
+//     }
+// }
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) =>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error);
+})
+.finally()
+
+//fetch , when we see in the terminal the outputs other asyncs and awaits are being displayed after the fetch output. :O
